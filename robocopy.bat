d8aca2bf33dc50d89981d02e63bb570e3773df29
Xor-van-Dorf <116817870+Xor-van-Dorf@users.noreply.github.com>
2023-08-09 11:12:33 -0400

    Update robocopy.bat

--- robocopy.bat
+++ robocopy.bat
@@ -3,3 +3,6 @@ robocopy SOURCE \\UNC /MIR /COPYALL /ZB /V /R:1 /W:1 /NP
 
 REM For Windows Server 2016 and up
 robocopy SOURCE \\UNC /MIR /COPYALL /ZB /V /R:1 /W:1 /NP /MT:32
+
+REM For Windows Server 2016 and up (TASK)
+robocopy SOURCE \\UNC /mt /z /e
